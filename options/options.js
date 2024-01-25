644f7a52f7d99109947c089bde60ffcd94f7bc5c
Crowdbotics <team@crowdbotics.com>
2024-01-25 23:56:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dermalink-45555",
+  url: "https://dermalink-45555.botics.co",
+  api: "https://dermalink-45555.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
